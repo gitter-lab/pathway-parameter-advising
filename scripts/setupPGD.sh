2524edf10584686092d37035bc252f070393e2cb
Chris Magnano <chrismagnano@gmail.com>
2019-11-13 14:09:51 -0600

    Changed PGA instillation to grab specific commit.

--- scripts/setupPGD.sh
+++ scripts/setupPGD.sh
@@ -14,6 +14,7 @@ mkdir ${homeDir}/lib/pgd;
 
 #Get PGD from github and compile it
 git clone https://github.com/nkahmed/PGD.git ${homeDir}/lib/pgd/;
+git --git-dir=${homeDir}/lib/pgd/.git checkout 05fef84db271554cc6ff2dbac9964c614e0c7981;
 make -C ${homeDir}/lib/pgd/;
 
 #Test run without saving any output
